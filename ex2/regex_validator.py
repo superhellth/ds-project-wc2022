77dbe1dccf4478b2eab4f614641aa5f7c855aca9
Bastian Müller <pz250@stud.uni-heidelberg.de>
2022-11-05 11:18:27 +0100

    Fix issue where unicode strings could not be handled. Refactor according to PEP8 guidelines.

--- ex2/regex_validator.py
+++ ex2/regex_validator.py
@@ -1,18 +1,23 @@
+# Globals
+VALID_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZÄÜÖß0123456789"
+VALID_SPECIAL = "^|*+?"
+VALID_BRACKETS = "()"
+TEST_EXPR_TRUE = ['Hallo mein Name ist (^)', u'\x46', 'Is THi5 tru3?', 'alpha']
+TEST_EXPR_FALSE = ['+-=005', u'\u2764', r'{}()[]', '<!=(`{mep']
 
-valid_char = "ABCDEFGHIJKLMNOPQRSTUVWXYZÄÜÖß0123456789"
-valid_special = "^|*+?"
-valid_brackets = "()"
-stack = []
 
 def split_string(string):
-        string_array = []
-        string_array.extend(string)
-        return string_array
+    string_array = []
+    string_array.extend(string)
+    return string_array
+
 
 def to_upper(char):
-    if(char != "ß"):
+    if char != "ß":
         return char.upper()
-    else: return char
+    else:
+        return char
+
 
 def upper_string(string):
     string_array = split_string(string)
@@ -20,46 +25,52 @@ def upper_string(string):
         string_array[i] = to_upper(string_array[i])
     return string_array
 
-def verify_regex(regex):
+
+def verify_regex(expr):
+    stack = []
     valid = False
-    if(regex==None):
+    # Handle potential unicode
+    expr = expr.encode('ascii', errors='backslashreplace')
+    expr = expr.decode('ascii')
+    if expr is None:
         return valid
-    elif(regex==""):
+    elif expr == "":
         valid = True
         return valid
     else:
-        regex = regex.replace(" ", "")
-        regex_array = upper_string(regex)
+        expr = expr.replace(" ", "")
+        regex_array = upper_string(expr)
         flag_special = False
         for char in regex_array:
-            if(valid_char.__contains__(char)==True):
+            if VALID_CHARS.__contains__(char):
                 flag_special = False
-            elif(valid_special.__contains__(char)==True):
-                if(flag_special == False):
+            elif VALID_SPECIAL.__contains__(char):
+                if not flag_special:
                     flag_special = True
                 else:
                     return valid
-            elif(valid_brackets.__contains__(char)==True):
-                if(char=="("):
+            elif VALID_BRACKETS.__contains__(char):
+                if char == "(":
                     stack.append("a")
-                elif(char==")" and len(stack)>0):
+                elif char == ")" and len(stack) > 0:
                     stack.pop()
                 else:
                     return valid
-            else: return valid
-    if(len(stack)==0):
+            else:
+                return valid
+    if len(stack) == 0:
         valid = True
         return valid
-    else: return valid
+    else:
+        return valid
 
 
+def test_regex_validator():
+    for test_exp in TEST_EXPR_TRUE:
+        assert (verify_regex(test_exp))
+    for test_exp in TEST_EXPR_FALSE:
+        assert (not verify_regex(test_exp))
 
 
-
-
-regex = "Hallo mein Name ist (^)"
-print(verify_regex(regex))
-
-
-
-
+if __name__ == "__main__":
+    test_regex_validator()
