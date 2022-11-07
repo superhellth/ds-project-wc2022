3a32cea22fcf5eead34ca608ff7c3fe0fdd2a2c2
Nico Hellthaler <nico.hellthaler@web.de>
2022-11-07 12:12:37 +0100

    Applied PEP8

--- ex2/regex_validator.py
+++ ex2/regex_validator.py
@@ -1,65 +1,68 @@
+"""This is our A1 T2 Regex validitor"""
 
-valid_char = "ABCDEFGHIJKLMNOPQRSTUVWXYZÄÜÖß0123456789"
-valid_special = "^|*+?"
-valid_brackets = "()"
+VALID_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZÄÜÖß0123456789"
+VALID_SPECIAL = "^|*+?"
+VALID_BRACKETS = "()"
 stack = []
 
+
 def split_string(string):
-        string_array = []
-        string_array.extend(string)
-        return string_array
-
-def to_upper(char):
-    if(char != "ß"):
-        return char.upper()
-    else: return char
-
-def upper_string(string):
-    string_array = split_string(string)
-    for i in range(len(string_array)):
-        string_array[i] = to_upper(string_array[i])
+    """Splits string ???"""
+    string_array = []
+    string_array.extend(string)
     return string_array
 
-def verify_regex(regex):
+
+def to_upper(char):
+    """Does nothing???"""
+    if char != "ß":
+        return char.upper()
+    else:
+        return char
+
+
+def upper_string(string):
+    """Converts string to array of upper case letters"""
+    string_array = split_string(string)
+    for i, char in enumerate(string_array):
+        string_array[i] = to_upper(char)
+    return string_array
+
+
+def verify_regex(expression: str):
+    """Our main verification method"""
     valid = False
-    if(regex==None):
+    if expression is None:
         return valid
-    elif(regex==""):
+    elif expression == "":
         valid = True
         return valid
     else:
-        regex = regex.replace(" ", "")
-        regex_array = upper_string(regex)
+        expression = expression.replace(" ", "")
+        regex_array = upper_string(expression)
         flag_special = False
         for char in regex_array:
-            if(valid_char.__contains__(char)==True):
+            if char in VALID_CHARS:
                 flag_special = False
-            elif(valid_special.__contains__(char)==True):
-                if(flag_special == False):
+            elif char in VALID_SPECIAL:
+                if flag_special is False:
                     flag_special = True
                 else:
                     return valid
-            elif(valid_brackets.__contains__(char)==True):
-                if(char=="("):
+            elif char in VALID_BRACKETS:
+                if char == "(":
                     stack.append("a")
-                elif(char==")" and len(stack)>0):
+                elif (char == ")" and len(stack) > 0):
                     stack.pop()
                 else:
                     return valid
-            else: return valid
-    if(len(stack)==0):
+            else:
+                return valid
+    if len(stack) == 0:
         valid = True
         return valid
-    else: return valid
+    return valid
 
 
-
-
-
-
-regex = "Hallo mein Name ist (^)"
-print(verify_regex(regex))
-
-
-
-
+REGEX_TO_CHECK = "Hallo mein Name ist (^)"
+print(verify_regex(REGEX_TO_CHECK))
