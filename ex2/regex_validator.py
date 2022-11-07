7bddec814e40c30f526ffc17fa16530c62a938df
Nico Hellthaler <nico.hellthaler@web.de>
2022-11-07 12:30:44 +0100

    cleaned up and commented code

--- ex2/regex_validator.py
+++ ex2/regex_validator.py
@@ -1,23 +1,24 @@
 """This is our regex validitor"""
 
 # Globals
-VALID_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZÄÜÖß0123456789"
+VALID_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZÄÜÖß0123456789 "
 VALID_SPECIAL = "^|*+?"
 VALID_BRACKETS = "()"
 TEST_EXPR_TRUE = [
-    'Hallo mein Name ist (^)', u'\x46', 'Is THi5 tru3?', 'alpha', 'ne(nu(un)e)n']
-TEST_EXPR_FALSE = ['+-=005', u'\u2764', r'{}()[]', '<!=(`{mep', '(123(te)']
+    'Hallo mein Name ist (^)', '\x46', 'Is THi5 tru3?', 'alpha', 'ne(nu(un)e)n', '', ""]
+TEST_EXPR_FALSE = ['+-=005', '\u2764',
+                   r'{}()[]', '<!=(`{mep', '(123(te)', None]
 
 
 def split_string(string):
-    """Splits string ???"""
+    """Splits string into char array"""
     string_array = []
     string_array.extend(string)
     return string_array
 
 
 def to_upper(char):
-    """Does nothing???"""
+    """Returns the uppercased letter"""
     if char != "ß":
         return char.upper()
     else:
@@ -34,42 +35,44 @@ def upper_string(string):
 
 def verify_regex(expr):
     """Our main verification method"""
+    # To keep track of brackets
     stack = []
-    valid = False
+
+    # Check if string is None
+    if expr is None:
+        return False
+
     # Handle potential unicode
     expr = expr.encode('ascii', errors='backslashreplace')
     expr = expr.decode('ascii')
-    if expr is None:
-        return valid
-    elif expr == "":
-        valid = True
-        return valid
-    else:
-        expr = expr.replace(" ", "")
-        regex_array = upper_string(expr)
-        flag_special = False
-        for char in regex_array:
-            if char in VALID_CHARS:
-                flag_special = False
-            elif char in VALID_SPECIAL:
-                if not flag_special:
-                    flag_special = True
-                else:
-                    return valid
-            elif char in VALID_BRACKETS:
-                if char == "(":
-                    stack.append("a")
-                elif char == ")" and len(stack) > 0:
-                    stack.pop()
-                else:
-                    return valid
+
+    # Pre-proccessing: Transform string to uppercased char array
+    regex_array = upper_string(expr)
+
+    # This flag is to make sure special characters don't appear immediately after each other
+    flag_special = False
+
+    # Iterate Char Array
+    for char in regex_array:
+        if char in VALID_CHARS:
+            flag_special = False
+        elif char in VALID_SPECIAL:
+            if not flag_special:
+                flag_special = True
             else:
-                return valid
+                return False
+        elif char in VALID_BRACKETS:
+            if char == "(":
+                stack.append("a")
+            elif char == ")" and len(stack) > 0:
+                stack.pop()
+            else:
+                return False
+        else:
+            return False
     if len(stack) == 0:
-        valid = True
-        return valid
-    else:
-        return valid
+        return True
+    return False
 
 
 def test_regex_validator():
