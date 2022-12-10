293ee7487c55066de46ce0b25179f49e8d8f216c
Bastian Müller <pz250@stud.uni-heidelberg.de>
2022-12-10 08:29:06 +0100

    Add animation (Mouseover), add image for TweetCard.svelte

--- src/frontend/src/svelte-components/TweetCard.svelte
+++ src/frontend/src/svelte-components/TweetCard.svelte
@@ -1,24 +1,46 @@
 <script lang="ts">
     import Tweet from "../typescript/tweet_management/tweet";
-    import {Card, CardBody, CardFooter, CardHeader, CardImg} from "sveltestrap";
+    import {Card, CardBody, CardFooter, CardHeader, CardImg, Col, Row} from "sveltestrap";
 
     export let data: Tweet;
+
+    let shadow = "";
+
+    function onOver() {
+        shadow = "shadow";
+    }
+
+    function onLeave() {
+        shadow = "";
+    }
+
 </script>
 
-<div>
-    <Card class="mt-4 mb-4">
-        <CardHeader>
-            <h5>
-                {data.getAuthor().getName()} (@{data.getAuthor().getUsername()})
-            </h5>
-        </CardHeader>
-        <CardBody>
-            {data.getText()}
-<!--            TODO: Insert image nicely-->
-<!--            <CardImg src={data.getAuthor().getProfileImageURL()}/>-->
-        </CardBody>
-        <CardFooter>
-            - {data.getAgeInHours()} minutes ago
-        </CardFooter>
+
+
+<div class="h-100">
+    <Card color="primary" outline class="mt-4 mb-4" >
+        <div on:mouseover={() => {onOver()}} class={shadow} on:mouseleave={() => {onLeave()}}>
+            <CardHeader>
+                <Row>
+                    <Col xs="2">
+                        <div>
+                            <CardImg src={data.getAuthor().getProfileImageURL()} />
+                        </div>
+                    </Col>
+                    <Col xs="10">
+                        <h5>
+                            {data.getAuthor().getName()} (@{data.getAuthor().getUsername()})
+                        </h5>
+                    </Col>
+                </Row>
+            </CardHeader>
+            <CardBody>
+                {data.getText()}
+            </CardBody>
+            <CardFooter>
+                - {data.getAgeInHours()} minutes ago
+            </CardFooter>
+        </div>
     </Card>
 </div>
