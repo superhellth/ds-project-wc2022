bd8300df88c7c43c8f771ce983727400e9777552
Nico Hellthaler <nico.hellthaler@web.de>
2022-12-18 11:53:55 +0100

    Refactor statistics code

---
+++ src/frontend/src/routes/statistics/graphs/TweetCountGraph.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+    import { Chart } from "chart.js/auto";
+    import ChartCard from "src/svelte-components/ChartCard.svelte";
+    import ElasticProvider from "src/typescript/api_connections/elasticProvider";
+    import { onMount } from "svelte";
+    import { Col, Input, Row } from "sveltestrap";
+
+    // data
+    let elasticProvider: ElasticProvider = ElasticProvider.getInstance();
+    let tweetsByDayPromise: Promise<Map<Date, number>> =
+        elasticProvider.getTweetsByDayStats();
+
+    // Chart
+    let chart: Chart;
+    let data: any = null;
+    const chart_id = "c1";
+    let header = "Tweets per Day";
+    let footer = "As we can see in this graph...";
+    let isBar: boolean = true;
+
+    function setupChart(canvas_id: any, type: any, data: any): Chart {
+        let chart = new Chart(canvas_id, {
+            type: type,
+            data: data,
+        });
+
+        return chart;
+    }
+
+    function onLineToggle() {
+        isBar = !isBar;
+
+        chart.destroy();
+        chart = setupChart(
+            chart_id,
+            isBar ? 'bar' : 'line',
+            data
+        );
+    }
+
+    onMount(async () => {
+        let tweetsByDayStats: Map<Date, number> = await tweetsByDayPromise;
+        data = {
+            labels: Array.from(tweetsByDayStats.keys()).map((date) =>
+                date.toLocaleDateString("short")
+            ),
+            datasets: [
+                {
+                    label: "Tweets",
+                    data: Array.from(tweetsByDayStats.values()),
+                },
+            ],
+        };
+        chart = setupChart(chart_id, "bar", data);
+    });
+</script>
+
+{#await tweetsByDayPromise}
+    <div class="d-flex justify-content-center mt-4">
+        <strong class="text-twitter-color">Loading Graph...</strong>
+        <div class="spinner-border ml-4 text-twitter-color" />
+    </div>
+{:then ignored}
+    <ChartCard {header} {footer} {chart_id}>
+        <div slot="card-header-controls">
+            <Row>
+                <Col>
+                    <Input
+                        type="switch"
+                        label="Line Graph"
+                        on:input={onLineToggle}
+                    />
+                </Col>
+            </Row>
+        </div>
+    </ChartCard>
+{/await}
