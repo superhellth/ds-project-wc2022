db7d1dea27e916a06b10a100351aedc4212e6c36
Nico Hellthaler <nico.hellthaler@web.de>
2023-01-10 12:21:08 +0100

    Add collocation generator

--- src/middleware/middleware/analysis/tweet_provider.py
+++ src/middleware/middleware/analysis/tweet_provider.py
@@ -1,18 +1,22 @@
+import queue
 from elasticsearch import helpers, Elasticsearch
 from middleware.tweet_management import twitter_tweet
 
 
 class TweetProvider:
+    """This class provides tweets from ES either as tweet objects or as strings."""
 
     def __init__(self):
         self.es_client = Elasticsearch(
             "http://45.13.59.173:9200", http_auth=("elastic", "sicheristsicher"))
         self.index = "tweets"
+        self.queue = queue.Queue()
 
     def get_tweet_list(self, size=100, body={"match_all": {}}):
         """Returns a list of tweet objects of size size."""
         tweets = list()
-        response = self.es_client.search(index=self.index, size=size, body=body, timeout="1m", request_timeout=30)
+        response = self.es_client.search(
+            index=self.index, size=size, body=body, timeout="1m", request_timeout=30)
         for tweet in response["hits"]["hits"]:
             tweets.append(twitter_tweet.Tweet(tweet, is_es_doc=True))
         return tweets
@@ -25,6 +29,44 @@ class TweetProvider:
             if len(corpus) == size:
                 break
         return corpus
+
+    def get_queue(self):
+        """Return queue object"""
+        return self.queue
+
+    def start_queue(self, batch_size=1000, max_tweets=-1):
+        """Start filling queue with tweet texts. Call this after starting worker threads on queue."""
+
+        batch_size = 1000
+        body = {
+            "query": {
+                "match_all": {}
+            },
+            "size": batch_size,
+            "_source": ["text"]
+        }
+        result = self.es_client.search(index="tweets", body=body, scroll="1m", timeout="1m", request_timeout=30)
+        scroll_id = result["_scroll_id"]
+
+        # Put the tweets in the queue and process them
+        tweets_loaded = 0
+        while True:
+            result = self.es_client.scroll(scroll_id=scroll_id, scroll="1m")
+            hits = result["hits"]["hits"]
+
+            if len(hits) == 0 or (max_tweets != -1 and tweets_loaded >= max_tweets):
+                break
+
+            for doc in hits:
+                tweet_text = doc["_source"]["text"]
+                self.queue.put(tweet_text)
+
+            tweets_loaded += batch_size
+            print(f"Tweets loaded: {tweets_loaded}")
+
+    def join_queue(self):
+        """Joins queue."""
+        self.queue.join()
 
     def get_client(self):
         """Returns a connection to the elasticsearch server"""
