b964fe88676a1d527daa1528bc71b407341419d6
Nico Hellthaler <nico.hellthaler@web.de>
2023-01-17 10:33:28 +0100

    Work on word graph

--- src/frontend/src/routes/word-graph/+page.svelte
+++ src/frontend/src/routes/word-graph/+page.svelte
@@ -1,29 +1,68 @@
 <script lang="ts">
     import { onMount } from "svelte";
     import Sigma from "sigma";
+    import Graph from "graphology";
+    import { Input } from "sveltestrap";
+    
+    let displayGraph: Graph;
+    let originalGraph: Graph = new Graph();
+    let threshold: number = 0;
     
     onMount(async () => {
-        
         const res = await fetch('collocations3.gexf');
         const text = await res.text();
 
         const Graph = await import("graphology").then(m => m.default)
         const { parse } = await import('graphology-gexf/browser');
-        const graph = parse(Graph, text);
+        displayGraph = parse(Graph, text);
+        originalGraph.import(displayGraph)
 
-        // Retrieve some useful DOM elements:
         const container = document.getElementById("sigma-container") as HTMLElement;
 
-        // Instanciate sigma:
-        const renderer = new Sigma(graph, container, {
-        minCameraRatio: 0.1,
-        maxCameraRatio: 10,
+        const renderer = new Sigma(displayGraph, container, {
+            minCameraRatio: 0.01,
+            maxCameraRatio: 10,
         });
     })
 
+    async function changeEdgeThreshold(threshold: number) {
+        if (displayGraph == null) {
+            return;
+        }
+
+        let maxWeight = -1
+
+        displayGraph.forEachEdge((edge) => displayGraph.dropEdge(edge))
+        displayGraph.forEachNode((node) => displayGraph.dropNode(node))
+        displayGraph.import(originalGraph.export())
+
+        displayGraph.forEachEdge((edge, attributes) => {
+            if (attributes.weight > maxWeight) {
+                maxWeight = attributes.weight;
+            }
+        })
+
+        let edgesToRemove = displayGraph.filterEdges((edge, attributes) => attributes.weight > maxWeight * threshold)
+        console.log(edgesToRemove.length)
+        
+        for (let edge in edgesToRemove) {
+            displayGraph.dropEdge(edge);
+        }
+    }
+
+    $: changeEdgeThreshold(threshold);
     
 </script>
 
+<Input
+      type="range"
+      name="range"
+      id="exampleRange"
+      min={0}
+      max={0.01}
+      step={0.000001}
+      bind:value={threshold}
+    />
 <div id="sigma-container"></div>
 
 <style>
