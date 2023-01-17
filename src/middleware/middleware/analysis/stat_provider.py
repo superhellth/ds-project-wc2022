b84b70f0b4560f67991aa8988075be3da49b85bf
Nico Hellthaler <nico.hellthaler@web.de>
2023-01-17 15:34:13 +0100

    Work on word graph

--- src/middleware/middleware/analysis/stat_provider.py
+++ src/middleware/middleware/analysis/stat_provider.py
@@ -8,9 +8,12 @@ class StatProvider:
     def __init__(self, path_to_data_files):      
         self.nlp = spacy.load("en_core_web_sm")
         self.n_gram_files = ["unigrams.json", "bigrams.json", "trigrams.json"]
+        self.collocation_files = ["collocations2.json", "collocations3.json", "collocation4.json"]
         self.path_to_data_files = path_to_data_files
         self.n_grams = [{}, {}, {}]
         self.sorted_n_grams = [[], [], []]
+        self.collocations_counts = [{}, {}, {}]
+        self.sorted_collocation_counts = [[], [], []]
 
     def load_n_grams(self, n):
         """Loads *grams.json into dict."""
@@ -18,17 +21,35 @@ class StatProvider:
         n_gram_dict = json.loads(f.read())
         self.n_grams[n-1] = n_gram_dict
 
+    def load_collocation_counts(self, window_size):
+        """Loads collocations*.json into dict."""
+        f = open(self.path_to_data_files + self.collocation_files[window_size - 2], "r", encoding="utf_8")
+        collocation_dict = json.loads(f.read())
+        self.collocations_counts[window_size - 2] = collocation_dict
+
     def get_n_grams_as_dict(self, n):
         """Return complete dict of n-gram counts."""
         if not self.n_grams[n-1]:
             self.load_n_grams(n)
         return self.n_grams[n-1]
 
+    def get_collocations_as_dict(self, window_size):
+        """Return complete dict of collocation counts."""
+        if not self.collocations_counts[window_size - 2]:
+            self.load_collocation_counts(window_size)
+        return self.collocations_counts[window_size - 2]
+
     def get_n_grams_as_list(self, n):
         """Return complete sorted list of n-grams including their counts."""
         if not self.sorted_n_grams[n-1]:
             self.sorted_n_grams[n-1] = sorted(self.get_n_grams_as_dict(n).items(), key=lambda x: x[1], reverse=True)
         return self.sorted_n_grams[n-1]
+
+    def get_collocations_as_list(self, window_size):
+        """Return complete sorted list of collocations including their counts."""
+        if not self.sorted_collocation_counts[window_size - 2]:
+            self.sorted_collocation_counts[window_size - 2] = sorted(self.get_collocations_as_dict(window_size).items(), key=lambda x: x[1], reverse=True)
+        return self.sorted_collocation_counts[window_size - 2]
 
     def get_n_gram_that(self, n, starts_with, not_in=[]):
         """Return n-gram string that starts with given string."""
