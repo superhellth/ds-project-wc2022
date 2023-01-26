4e3c320cc31a7f36c96d98dd0ed61486d61884ec
Bastian Müller <pz250@stud.uni-heidelberg.de>
2023-01-26 17:35:55 +0100

    Add vaderSentiment to GUI. Add base for BERT and Naive Bayes classifier.

---
+++ src/middleware/middleware/analysis/sentiment_NB.py
@@ -0,0 +1,11 @@
+from abc import ABC
+from typing import List, Tuple, Dict
+
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from middleware.analysis.sentiment_base import SentimentBase
+
+
+class NBSentiment(SentimentBase, ABC):
+    def __init__(self, model_name):
+        super().__init__(model_name)
+        self.model = SentimentIntensityAnalyzer()
