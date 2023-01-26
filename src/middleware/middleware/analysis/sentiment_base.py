ab938b0f27db90dde3ebd769f6c5a7bc2a9c9427
Bastian Müller <pz250@stud.uni-heidelberg.de>
2023-01-26 08:28:10 +0100

    Add sentiment analysis classes.

---
+++ src/middleware/middleware/analysis/sentiment_base.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+
+class SentimentBase(ABC):
+    def __init__(self, model_name):
+        self.did_train = False
+        self.model_name = model_name
+
+    @abstractmethod
+    def get_sentiment_of_text(self, text):
+        pass
+
+    @abstractmethod
+    def get_sentiment_of_text_list(self, texts):
+        pass
+
+    @abstractmethod
+    def get_average_sentiment_of_text_list(self, texts):
+        pass
+
+    @abstractmethod
+    def get_sentiment_of_text_list_by_date(self, texts):
+        pass
+
+    @abstractmethod
+    def train_and_save_model(self):
+        pass
