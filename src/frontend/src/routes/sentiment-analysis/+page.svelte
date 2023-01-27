e3d251aae9c67f3dfa70dfe43b4f82825b0a868d
Bastian Müller <pz250@stud.uni-heidelberg.de>
2023-01-27 14:58:35 +0100

    Add empty sentiment page.

---
+++ src/frontend/src/routes/sentiment-analysis/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import Loading from "src/svelte-components/Loading.svelte";
+    import ElasticProvider from "src/typescript/api_connections/elasticProvider";
+    import { onMount } from "svelte";
+    import { Button, Form, FormGroup, Input, Tooltip } from "sveltestrap";
+
+    let provider: ElasticProvider = ElasticProvider.getInstance();
+
+</script>
+
+<title>Text Analytics - Sentiment Analysis</title>
+
+<div>
+
+</div>
