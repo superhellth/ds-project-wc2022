6273bbef5cce6b88a95181d214d769963100ad44
Raphael Ebner <ve257@stud.uni-heidelberg.de>
2023-02-04 15:05:36 +0100

    Finished TF_IDF Class. Added function to get the words from the corpus with the highest average and total sum of tfidf scores

--- src/middleware/middleware/analysis/test_tfidf.py
+++ src/middleware/middleware/analysis/test_tfidf.py
@@ -10,6 +10,7 @@ class Test_TF_IDF:
         self.corpus = None
         self.tf_idf_matrix = None
         self.similarity_matrix = None
+        self.vocabulary = None
 
     def calculate_tf_idf(self, num_tweets=1, min_df=3,max_df=0.85):
         self.corpus = self.corpus_analyzer.generate_tokenized_tweets(num_tweets=num_tweets)
@@ -18,6 +19,7 @@ class Test_TF_IDF:
         # Count term appearances
         count_vectorizer = CountVectorizer()
         count_matrix = count_vectorizer.fit_transform(self.corpus)
+        self.vocabulary = count_vectorizer.vocabulary_
 
         # Calculate tf-idf values
         tf_idf_transformer = TfidfTransformer()
@@ -30,7 +32,7 @@ class Test_TF_IDF:
         self.similarity_matrix = cosine_similarity(self.tf_idf_matrix)
         return self.similarity_matrix
 
-    def n_highest_tfidf(self,n=10):
+    def n_highest_mean_tfidf(self,n=10):
         A = np.array(self.tf_idf_matrix)
         # calculate the average value of each column
         column_averages = np.mean(A, axis=0)
@@ -40,27 +42,62 @@ class Test_TF_IDF:
 
         # get the 5 columns of A with the highest average value
         most_average_columns = A[:, indices]
+        # calculate the mean of the most average columns
+        mean_of_most_average_columns = np.mean(most_average_columns,axis=0)
 
-        # print the 5 columns of A with the highest average value
-        print(np.mean(most_average_columns, axis=0))
-        return most_average_columns
+        most_average_words = [word for word, index in self.vocabulary.items() if index in indices]
+        result = [[most_average_words[i], mean_of_most_average_columns[i]] for i in range(n)]
+        return result
+    
+    def n_highest_sum_tfidf(self,n=10):
+        A = np.array(self.tf_idf_matrix)
+        # calculate the sum value of each column
+        column_averages = np.sum(A, axis=0)
+
+        # find the indices of the 5 largest elements in the column_sum array
+        indices = np.argpartition(column_averages, -n)[-n:]
+
+        # get the 5 columns of A with the highest sum value
+        most_sum_columns = A[:, indices]
+        # calculate the mean of the most sum columns
+        total_of_most_sum_columns = np.sum(most_sum_columns,axis=0)
+
+        most_sum_words = [word for word, index in self.vocabulary.items() if index in indices]
+        result = [[most_sum_words[i], total_of_most_sum_columns[i]] for i in range(n)]
+        return result
 
     def most_similar_tweets(self,n=20):
         k = []
         sorted_k = []
 
-        if self.similarity_matrix == None:
-            self.similarity_matrix = self.calculate_cosine_similarity(num_tweets = 1, min_df=5,max_df=0.85)
         for i in range(len(self.similarity_matrix)):
             for j in range(i+1,len(self.similarity_matrix[0])):
-                if self.similarity_matrix[i][j] >= 1 and i != j:
+                if i != j:
                     if self.corpus[i] != self.corpus[j]:
                         k.append([self.corpus[i], self.corpus[j], self.similarity_matrix[i][j]])
 
         # sort k based on the third element (self.similarity_matrix[i][j]) in descending order
         sorted_k = sorted(k, key=lambda x: x[2], reverse=True)
 
-        # keep only the 20 highest values of matrix[i][j]
+        # keep only the n highest values of matrix[i][j]
+        sorted_k = sorted_k[:n]
+
+        return sorted_k
+
+    def least_similar_tweets(self,n=20):
+        k = []
+        sorted_k = []
+
+        for i in range(len(self.similarity_matrix)):
+            for j in range(i+1,len(self.similarity_matrix[0])):
+                if i != j:
+                    if self.corpus[i] != self.corpus[j]:
+                        k.append([self.corpus[i], self.corpus[j], self.similarity_matrix[i][j]])
+
+        # sort k based on the third element (self.similarity_matrix[i][j]) in ascending order
+        sorted_k = sorted(k, key=lambda x: x[2])
+
+        # keep only the n lowest values of matrix[i][j]
         sorted_k = sorted_k[:n]
 
         return sorted_k
--- src/middleware/middleware/analysis/testingground.py
+++ src/middleware/middleware/analysis/testingground.py
@@ -1,17 +1,14 @@
 from middleware.analysis import test_tfidf
 import numpy as np
 tfidf = test_tfidf.Test_TF_IDF()
-matrix = tfidf.calculate_cosine_similarity()
+matrix = tfidf.calculate_cosine_similarity(num_tweets=1000)
 
-k=[]
-for i in range(len(matrix)):
-    for j in range(i+1,len(matrix[0])):
-        if matrix[i][j] >=1 and i!=j:
-            if tfidf.corpus[i]!=tfidf.corpus[j]:
-                k.append([tfidf.corpus[i],tfidf.corpus[j],matrix[i][j]])
+"""k=tfidf.most_similar_tweets()
 for i in range(len(k)):
     print("____________________")
     print(k[i])
     print("____________________")
 print(len(k))
-#x = tfidf.n_highest_tfidf()
+print(tfidf.vocabulary)"""
+x= tfidf.n_highest_sum_tfidf()
+print(x)
