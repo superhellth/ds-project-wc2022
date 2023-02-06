e1e33bb6d5f5832beb53120b22944c0f3c070f97
Nico Hellthaler <nico.hellthaler@web.de>
2023-02-06 17:28:58 +0100

    Work on home page, please try out

--- src/middleware/middleware/analysis/playground.py
+++ src/middleware/middleware/analysis/playground.py
@@ -1,17 +1,15 @@
-import ujson
+from middleware.analysis import tweet_provider
 
-converted_dict = dict()
-with open("./src/data/nes.json", "r", encoding="utf_8") as f:
-    ne_dict = ujson.loads(f.read())
+provider = tweet_provider.TweetProvider()
+es_client = provider.get_client()
+resp = es_client.search(index="tweets", body={
+    "aggs": {
+        "type_count": {
+            "cardinality": {
+                "field": "author.location"
+            }
+        }
+    }
+}, timeout="1m", request_timeout=120, size=0)
 
-for item in ne_dict.items():
-    key_string = item[0]
-    parts = key_string.split(",")
-    ne = parts[0][2:len(parts[0]) - 1]
-    type = parts[1][2:len(parts[1]) - 2]
-    converted_dict[(ne, type)] = item[1]
-
-sorted_list = sorted(ne_dict.items(), key=lambda x: x[1], reverse=True)
-print(sorted_list[0:20])
-sorted_list = sorted(converted_dict.items(), key=lambda x: x[1], reverse=True)
-print(sorted_list[0:20])
+print(resp)
