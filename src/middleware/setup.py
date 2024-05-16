b75c5953d6e5b147214cde068194a941f32f3655
Nico Hellthaler <nico.hellthaler@web.de>
2024-05-16 12:49:14 +0200

    Specify depency versions

--- src/middleware/setup.py
+++ src/middleware/setup.py
@@ -6,8 +6,8 @@ setup(name='middleware',
                 'middleware.data_retrieval',
                 'middleware.tweet_management',
                 'middleware.analysis'],
-      install_requires=['tweepy', 'elasticsearch==7.17', 'fastapi[all]', 'pandas', 'gensim', 'matplotlib',
-                        'scikit-learn', 'numpy', 'seaborn', 'tk', 'ujson', 'networkx', 'node2vec',
-                        'vaderSentiment', 'transformers', 'torch==2.0.0', 'torchvision==0.15.1', 'gensim']
+      install_requires=['tweepy==4.13.0', 'elasticsearch==7.17', 'fastapi[all]==0.93.0', 'pandas==1.5.3', 'gensim==4.3.1', 'matplotlib==3.7.1',
+                        'scikit-learn==1.2.2', 'numpy==1.24.2', 'seaborn==0.12.2', 'tk', 'ujson==5.7.0', 'networkx==3.0', 'node2vec==0.4.6',
+                        'vaderSentiment==3.3.2', 'transformers==4.27.0', 'torch==2.0.0', 'torchvision==0.15.1', 'scipy==1.10.1']
 
       )
